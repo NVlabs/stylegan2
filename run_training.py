244e7a42f6a546284df2251e51aa9f650261b9bc
Derrick Schultz <dvsmethid@gmail.com>
2019-12-18 00:05:44 -0500

    rollback minibatch schedules

--- run_training.py
+++ run_training.py
@@ -58,7 +58,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     train.network_snapshot_ticks = 2
     sched.G_lrate_base = sched.D_lrate_base = 0.002
     sched.minibatch_size_base = 32
-    sched.minibatch_gpu_base = 8
+    sched.minibatch_gpu_base = 4
     D_loss.gamma = 10
     metrics = [metric_defaults[x] for x in metrics]
     desc = 'stylegan2'
