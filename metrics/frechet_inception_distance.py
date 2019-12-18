7e5374e967d411f01a4fb08c11ed546e05841056
paperspace <dvsmethid@gmail.com>
2019-12-18 11:08:07 -0500

    small fixes for metrics

--- metrics/frechet_inception_distance.py
+++ metrics/frechet_inception_distance.py
@@ -25,7 +25,7 @@ class FID(metric_base.MetricBase):
 
     def _evaluate(self, Gs, Gs_kwargs, num_gpus):
         minibatch_size = num_gpus * self.minibatch_per_gpu
-        inception = misc.load_pkl('./pkls/inception_v3_features.pkl') # inception_v3_features.pkl
+        inception = misc.load_pkl('./pkl/inception_v3_features.pkl') # inception_v3_features.pkl
         activations = np.empty([self.num_images, inception.output_shape[1]], dtype=np.float32)
 
         # Calculate statistics for reals.
--- training/training_loop.py
+++ training/training_loop.py
@@ -339,7 +339,8 @@ def training_loop(
             if network_snapshot_ticks is not None and (cur_tick % network_snapshot_ticks == 0 or done):
                 pkl = dnnlib.make_run_dir_path('network-snapshot-%06d.pkl' % (cur_nimg // 1000))
                 misc.save_pkl((G, D, Gs), pkl)
-                #metrics.run(pkl, run_dir=dnnlib.make_run_dir_path(), data_dir=dnnlib.convert_path(data_dir), num_gpus=num_gpus, tf_config=tf_config)
+                if network_snapshot_ticks is not None and (cur_tick % (network_snapshot_ticks*4) == 0): 
+                    metrics.run(pkl, run_dir=dnnlib.make_run_dir_path(), data_dir=dnnlib.convert_path(data_dir), num_gpus=num_gpus, tf_config=tf_config)
 
             # Update summaries and RunContext.
             metrics.update_autosummaries()
