b14f975b95da05540f4f025e9f6bc3f9393762f4
Derrick Schultz <dvsmethid@gmail.com>
2020-05-07 23:02:01 -0400

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -362,6 +362,7 @@ def _parse_num_range(s):
 
 def _parse_npy_files(files):
     '''Accept a comma separated list of npy files and return a list of z vectors.'''
+    print(files)
     zs =[]
     
     for f in files:
